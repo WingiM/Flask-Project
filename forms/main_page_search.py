0574bacfa95345a593462a3ae21a5ca1d4ba1063
WingiM <wingimobile@gmail.com>
2021-04-18 10:06:27 +0300

    Made main page

--- forms/main_page_search.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class AddProductForm(FlaskForm):
-    to_find = StringField(validators=[DataRequired()])
-    submit = SubmitField('Поиск по сайту')
